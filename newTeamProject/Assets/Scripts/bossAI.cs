450e7ec609045b1076cfa9a160cf221c5000e9cc
patrickjhand <phand1216@outlook.com>
2023-09-23 20:33:26 -0400

    Fixed build issues

--- newTeamProject/Assets/Scripts/bossAI.cs
+++ newTeamProject/Assets/Scripts/bossAI.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.ProjectWindowCallback;
+//using UnityEditor.ProjectWindowCallback;
 using UnityEngine;
 using UnityEngine.AI;
 
