2595850f5f141743456ef76920a162270de3b162
Ruddy Abreu <RAbreu1@student.fullsail.edu>
2023-10-08 21:40:04 -0400

    boss and enemy ui

---
+++ newTeamProject/Assets/Scripts/UiEnemyHealthBar.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiEnemyHealthBar : MonoBehaviour
+{
+     Slider slider;
+    float timeBarhidden;
+
+    private void Awake()
+    {
+        slider = GetComponentInChildren<Slider>();
+    }
+    public void SetHealth(int HP)
+    {
+        slider.value = HP;
+        timeBarhidden = 3;
+    }
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+    private void Update()
+    {
+        timeBarhidden = timeBarhidden - Time.deltaTime;
+        if (slider != null)
+        {
+
+            if (timeBarhidden <= 0)
+            {
+                timeBarhidden = 0;
+                slider.gameObject.SetActive(false);
+
+            }
+
+            else
+            {
+                if (!slider.gameObject.activeInHierarchy)
+                {
+                    slider.gameObject.SetActive(true);
+                }
+            }
+
+            if (slider.value <= 0)
+
+            {
+
+                Destroy(slider.gameObject);
+
+            }
+        }
+
+    }
+
+}
