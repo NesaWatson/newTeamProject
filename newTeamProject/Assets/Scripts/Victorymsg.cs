cd639a76132ed4b3e2b4a92f87f86cdf2c2e2bc0
Ruddy Abreu <RAbreu1@student.fullsail.edu>
2023-10-14 15:28:07 -0400

    enemy ui's victory message script, and level

---
+++ newTeamProject/Assets/Scripts/Victorymsg.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Victorymsg : MonoBehaviour
+{
+    public Text victoryText;
+    public float victoryduration = 5.0f;
+    public void Start()
+    {
+        victoryText.enabled = false;
+    }
+public void ShowVictoryMessage()
+    {
+
+    }
+    IEnumerator Displaymsg()
+    {
+        victoryText.enabled=true;
+        yield return new WaitForSeconds(victoryduration);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
